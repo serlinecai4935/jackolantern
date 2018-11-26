288658ce0175fef28d04355862d2c86af96ced12
BT_1N3_14@BKTECH.LOCAL <BT_1N3_14@BKTECH.LOCAL>
2018-11-26 14:45:23 -0500

    Initial commit

---
+++ src/JackoLanternRunner.java
@@ -0,0 +1,9 @@
+public class JackoLanternRunner {
+    public static void main(String[]args)
+    {
+        String[][]face = new String [5][8];
+        JackOLantern face1= new JackOLantern(face);
+
+        face1.fill
+    }
+}
