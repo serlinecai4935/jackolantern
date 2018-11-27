3f9ad3523e52c4d36087fd4c2302447f15f0224a
serlinecai4935 <scai4935@bths.edu>
2018-11-27 14:46:17 -0500

    g

--- src/JackoLanternRunner.java
+++ src/JackoLanternRunner.java
@@ -1,9 +1,26 @@
+//serline cai
 public class JackoLanternRunner {
     public static void main(String[]args)
     {
         String[][]face = new String [5][8];
         JackOLantern face1= new JackOLantern(face);
 
-        face1.fill
+        face1.fill(str);
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        System.out.println(face1);
     }
 }
